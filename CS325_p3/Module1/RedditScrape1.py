5a252ecdb321f4321d4474ba323a89a70c6601d1
jpphil0 <jpphil0@charter.net>
2023-11-16 10:33:24 -0600

    reverted to project4

--- CS325_p3/Module1/RedditScrape1.py
+++ CS325_p3/Module1/RedditScrape1.py
@@ -10,41 +10,19 @@ import os
 from Module2.RedditScrape2 import download_reddit_content
 from Module3.ExtractComments import extract_comments
 
-def sanitize_filename(filename):
-    # Replace spaces with underscores and handle other problematic characters
-    invalid_chars = r'\/:*?"<>|'
-    sanitized_filename = "".join(c if c.isalnum() or c in ('_', '.', ' ') else '_' for c in filename)
-    sanitized_filename = "".join(c if c not in invalid_chars else '_' for c in sanitized_filename)
-    sanitized_filename = sanitized_filename.strip('_')  # Remove underscores from the ends
-    return sanitized_filename.replace(' ', '_')
-
-def process_urls_from_file(input_file):
-    try:
-        with open(input_file, 'r', encoding='utf-8') as url_file:
-            urls = url_file.read().splitlines()
-
-        for idx, url in enumerate(urls, start=1):
-            # Create a unique raw filename based on the order of input
-            raw_filename = f"raw_{idx}.txt"
-            output_file_raw = os.path.join("Data", "raw", raw_filename)
-
-            # Download Reddit content and save it to a unique raw file
-            post_title = download_reddit_content(url, output_file_raw)
-
-            # Extract comments from the raw file and save them to a processed file
-            sanitized_title = sanitize_filename(post_title)
-            output_file_processed = os.path.join("Data", "processed", f"{sanitized_title}_comments.txt")
-            extract_comments(output_file_raw, output_file_processed)
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python RedditScrape1.py <input_urls_file.txt>")
+    if len(sys.argv) != 3:
+        print("Usage: python file1.py <URL> <output_file.txt>")
     else:
-        input_file = sys.argv[1]
-        process_urls_from_file(input_file)
+        reddit_url = sys.argv[1]
+        output_file_raw = os.path.join("Data", "raw", sys.argv[2])
+
+        # Download Reddit content and save it to raw file
+        download_reddit_content(reddit_url, output_file_raw)
+
+        # Extract comments from the raw file and save them to processed file
+        output_file_processed = "Data/processed/comments.txt"
+        extract_comments(output_file_raw, output_file_processed)
 
 if __name__ == "__main__":
     main()
